71f8e80859f2187f981793cb6dbc0b357f15f490
Chao Liu <yijinliu@gmail.com>
2018-08-07 22:29:29 -0700

    Install libcupti-dev in nvidia.sh and add scripts to user cuda docker containers.

--- system/nvidia.sh
+++ system/nvidia.sh
@@ -25,5 +25,29 @@ if [ $rc != 0 ]; then
     exit 1
 fi
 
+sudo apt install -y libcupti-dev
+rc=$?
+if [ $rc != 0 ]; then
+    echo -e "${RED}Failed to install libcupti-dev!${NC}"
+    exit 1
+fi
+
+# See https://devblogs.nvidia.com/gpu-containers-runtime/
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - &&
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list &&
+sudo apt update &&
+sudo apt install -y nvidia-docker2 &&
+sudo pkill -SIGHUP dockerd
+rc=$?
+if [ $rc != 0 ]; then
+    echo -e "${RED}Failed to install nvidia-docker2!${NC}"
+    exit 1
+fi
+
+sudo nvidia-container-cli --load-kmods info
+# See https://hub.docker.com/r/nvidia/cuda/ and https://ngc.nvidia.com/registry/nvidia-tensorflow
+# for prebuilt containers.
+
 export PATH=/usr/local/cuda/bin:${PATH}
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:${LD_LIBRARY_PATH}
